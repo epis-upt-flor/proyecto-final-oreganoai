eabd01075e60a4dc8082c02f221c51bdc267b667
MarceloCuadros <rc2017057851@virtual.upt.pe>
2025-06-23 22:56:05 -0500

    Actualizando MVVM

--- organoai/lib/datos/conexionApi.dart
+++ organoai/lib/datos/conexionApi.dart
@@ -3,18 +3,18 @@ import 'package:http/http.dart' as http;
 
 class ConexionApi {
   // URL de la API REST
-  final String _url = 'https://flask-cnn-api-latest.onrender.com/predict';
+  final String _url = 'https://e85f-138-99-179-97.ngrok-free.app/predict';
 
   // Función para subir la imagen y obtener la respuesta de la API
   Future<Map<String, dynamic>> predictImage(String imagePath) async {
     var request = http.MultipartRequest('POST', Uri.parse(_url));
     // Adjuntar la imagen al request
     request.files.add(await http.MultipartFile.fromPath('file', imagePath));
-    
+
     // Enviar la solicitud HTTP
     var streamedResponse = await request.send();
     var response = await http.Response.fromStream(streamedResponse);
-    
+
     // Comprobar el estado y decodificar la respuesta
     if (response.statusCode == 200) {
       return json.decode(response.body) as Map<String, dynamic>;
